91b1ab093358eb49ff423c521182286ed9c15e35
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-08-07 09:20:03 +0530

    Correction done in controller file

--- app/controller/note.controller.js
+++ app/controller/note.controller.js
@@ -4,23 +4,23 @@ exports.create = (req, res) => {
   if (!req.body.content) {
     return res.status(400).send({ message: "Note content cannot be empty" });
   }
-};
 
-const note = new note({
-  title: req.body.title || "Untitled Note",
-  content: req.body.content,
-});
-
-note
-  .save()
-  .then((data) => {
-    res.send(data);
-  })
-  .catch((err) => {
-    res.status(500).send({
-      message: err.message || "Some error occurred while creating the Note.",
-    });
+  note = new note({
+    title: req.body.title || "Untitled Note",
+    content: req.body.content,
   });
+
+  note
+    .save()
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while creating the Note.",
+      });
+    });
+};
 
 exports.findAll = (req, res) => {};
 exports.findOne = (req, res) => {};
